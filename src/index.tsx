a3c81628cfec95f8b450742712e4608112da10cc
Matheus Schiffer <matheusschiffer@gmail.com>
2022-01-07 14:35:14 -0300

    reducer

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import userStore from './redux/userStore';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={userStore}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
