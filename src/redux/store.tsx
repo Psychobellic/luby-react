9d3c552acd79ff51128fca5efe922773149af425
Matheus Schiffer <matheusschiffer@gmail.com>
2022-01-07 13:55:44 -0300

    store1

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
+    /*
     email: emailReducer,
     password: passwordReducer,
     token: tokenReducer,
     remember: rememberReducer,
+    */
   }
 });
 
